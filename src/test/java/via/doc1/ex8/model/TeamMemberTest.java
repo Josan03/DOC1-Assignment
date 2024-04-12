514ffe975bb2d697d15f3e336fca3c2f7b87fa39
Cristian Josan <josan.c2003@gmail.com>
2024-04-12 08:50:19 +0200

    Add tests to the model class TeamMember.

--- src/test/java/via/doc1/ex8/model/TeamMemberTest.java
+++ src/test/java/via/doc1/ex8/model/TeamMemberTest.java
@@ -22,9 +22,31 @@ public class TeamMemberTest {
     }
 
     @Test
-    public void getTeamMemberTest() {
+    public void getTeamMember_Test() {
         assertEquals("Member1", testTeamMember.getId());
         assertEquals("Johnny", testTeamMember.getName());
         assertEquals("johnny@gmail.com", testTeamMember.getEmail());
+    }
+
+    @Test
+    public void setTestTeamMember_Test() {
+        String id = "Member2";
+        String name = "Jane";
+        String email = "jane@gmail.com";
+        testTeamMember.setId(id);
+        testTeamMember.setName(name);
+        testTeamMember.setEmail(email);
+        assertEquals(id, testTeamMember.getId());
+        assertEquals(name, testTeamMember.getName());
+        assertEquals(email, testTeamMember.getEmail());
+    }
+
+    @Test
+    public void setTasks_getTasks_Test() {
+        Task task3 = new Task("Task3", "Task3 Description", "Task3 Status");
+        Task task4 = new Task("Task4", "Task4 Description", "Task4 Status");
+        List<Task> tasks = List.of(task3, task4);
+        testTeamMember.setTasks(tasks);
+        assertEquals(tasks, testTeamMember.getTasks());
     }
 }
